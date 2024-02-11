203135f59fea4cffb264da11346747201b21e529
Maher Kefi <kefi.maher@esprit.tn>
2024-02-11 20:44:58 +0100

    maher

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FailComponent } from './fail/fail.component';
+import { LoginComponent } from './login/login.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
-  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'', component:LoginComponent},
+  {path:'formulaire', component:FormulaireComponent},
   {path:'**', component:FailComponent}
+
 ];
-
-
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FailComponent } from './fail/fail.component';
+import { LoginComponent } from './login/login.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FailComponent
+    FailComponent,
+    LoginComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
